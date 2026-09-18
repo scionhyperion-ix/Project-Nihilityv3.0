ad825119bcb7b037dee3503a1f982fa80e58b8c3
Jonathan Crane <jonathancraneofficial@gmail.com>
2026-09-19 02:15:49 +0800

    Switch media uploads to Supabase Storage

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,4 @@
 window.NIHILITY_CONFIG={
   SUPABASE_URL:"https://oshuprrppelxcxfmcumm.supabase.co",
-  SUPABASE_ANON_KEY:"sb_publishable_c1eacVHrCTWQT3zG6RtBKg_bqScW4kh",
-  R2_WORKER_URL:""
+  SUPABASE_ANON_KEY:"sb_publishable_c1eacVHrCTWQT3zG6RtBKg_bqScW4kh"
 };
