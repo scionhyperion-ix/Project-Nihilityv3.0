37e300fbfbadeeaf814417d06390e947b52e568f
Papi Crane <jonathancraneofficial@gmail.com>
2026-09-25 18:44:05 +0800

    Avoid startup repaint from Timeline

--- assets/js/system-timeline.js
+++ assets/js/system-timeline.js
@@ -21,7 +21,7 @@
     state.timelineEvents=rows;
     state.timelineLoaded=rows.length;
     state.timelineHasMore=rows.length===PAGE_SIZE;
-    if(!window.nihilitySilentRefresh)renderTimeline();
+    if(!window.nihilitySilentRefresh&&!window.nihilityInitialHydration)renderTimeline();
   };
 
   function memberNameById(id){
